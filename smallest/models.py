1ccea61c3b104616dc8863cb824ef8bd426e523f
hamees-sayed <hameessayed71@gmail.com>
2024-11-10 20:31:51 +0000

    added new voices

--- smallest/models.py
+++ smallest/models.py
@@ -2,4 +2,6 @@ from typing import Literal
 
 TTSModels = Literal["lightning"]
 TTSLanguages = Literal["en", "hi"]
-TTSVoices = Literal["emily", "jasmine", "arman", "james", "mithali", "aravind", "raj"]
+TTSVoices = Literal["emily", "jasmine", "arman", "james", "mithali", "aravind", "raj", 
+                    "arjun", "sanya", "saina", "pooja", "saurabh", "nisha", "mansi", "radhika", "kajal", 
+                    "raghav", "deepika", "niharika", "monika", "raman", "diya", "ananya", "william", "isha"]
