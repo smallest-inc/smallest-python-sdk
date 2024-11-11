0907882f5dd077d815801a86302b4e70744da6a4
hamees-sayed <hameessayed71@gmail.com>
2024-11-11 05:34:15 +0000

    voices

--- smallest/models.py
+++ smallest/models.py
@@ -4,4 +4,4 @@ TTSModels = Literal["lightning"]
 TTSLanguages = Literal["en", "hi"]
 TTSVoices = Literal["emily", "jasmine", "arman", "james", "mithali", "aravind", "raj", 
                     "arjun", "sanya", "saina", "pooja", "saurabh", "nisha", "mansi", "radhika", "kajal", 
-                    "raghav", "deepika", "niharika", "monika", "raman", "diya", "ananya", "william", "isha"]
+                    "raghav", "deepika", "niharika", "monika", "raman", "diya", "ananya", "william"]
