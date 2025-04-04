cfcd9a3d2d753d9046a70fabb1ae3bae0006b070
utkarsh <utkarshm065@gmail.com>
2025-04-04 16:42:38 +0530

    clean up ...

--- tests/atoms/test_calls_api.py
+++ tests/atoms/test_calls_api.py
@@ -7,7 +7,7 @@ def test_start_outbound_call(atoms_client, global_state):
     
     request = StartOutboundCallRequest(
         agentId=global_state["base_agent"]["id"],
-        phoneNumber="+919769384457",
+        phoneNumber="+919666666666",
     )
     
     response = atoms_client.start_outbound_call(start_outbound_call_request=request)
