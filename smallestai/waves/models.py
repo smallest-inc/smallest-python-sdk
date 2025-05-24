12616d44bc1592b80e6581b9003bc274f6f0d48b
Hamees Sayed <98336593+hamees-sayed@users.noreply.github.com>
2025-05-24 15:37:27 +0530

    Update models.py

--- smallestai/waves/models.py
+++ smallestai/waves/models.py
@@ -1,6 +1,6 @@
 TTSLanguages_lightning = ["en", "hi"]
 TTSLanguages_lightning_large = ["en", "hi"]
-TTSLanguages_lightning_v2 = ["en", "hi", "mr", "kn", "ta", "bn", "gj", "de", "fr", "es", "it", "pl", "nl", "ru", "ar", "he"]
+TTSLanguages_lightning_v2 = ["en", "hi", "mr", "kn", "ta", "bn", "gu", "de", "fr", "es", "it", "pl", "nl", "ru", "ar", "he"]
 TTSModels = [
     "lightning", 
     "lightning-large",
